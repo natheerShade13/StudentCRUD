0f0663522694725aec7a852b60444f0025cbc9a9
natheerShade13 <129689662+natheerShade13@users.noreply.github.com>
2024-03-24 03:13:06 +0200

    Create Functionality (#1)

--- src/main/java/za/ac/cput/repository/StudentRepository.java
+++ src/main/java/za/ac/cput/repository/StudentRepository.java
@@ -1,9 +1,17 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Student;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    //SELECT * FROM student WHERE email = ?
+    @Query("SELECT s FROM Student s WHERE s.email = ?1") //Query
+    Optional<Student> findStudentByEmail(String email);
+
 }
